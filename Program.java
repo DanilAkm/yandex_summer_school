2f6bc1138ed4858c35c1f78c1484fb9c340aa720
DanilAkm <97963138+DanilAkm@users.noreply.github.com>
2023-05-02 02:54:00 +0500

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-
+// biba i boba
 class Program {
   public static void main(String[] args) throws IOException {
 
